7f473caab7a2d12e04e1c61685983e71f377408c
Mani Shankar <vmshankar20@gmail.com>
2018-12-17 12:39:41 +0530

    Snippet: For logging both on text and console of python

---
+++ logging_with_console.py
@@ -0,0 +1,23 @@
+""" 
+Logging pacakge to log the info both on text and console for readability purpose.
+This is well use for run time debugging on realtime.
+"""
+
+import logging
+import datetime
+
+# set up logging to file
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename='./logs/test_log_{0}.log'.format(datetime.datetime.now().strftime('%Y%m%d_%H%M%S')),
+                    filemode='w')
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+# tell the handler to use this format
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
